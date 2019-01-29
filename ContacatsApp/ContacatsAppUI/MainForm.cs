e0050f611b43122261fffdba7308fba5a5b9ea34
suff4egg <46990184+suff4egg@users.noreply.github.com>
2019-01-29 15:07:34 +0700

    Класс с главным окном переименован

--- ContacatsApp/ContacatsAppUI/MainForm.cs
+++ ContacatsApp/ContacatsAppUI/MainForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace ContacatsAppUI
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
